2ee6637a4d977930483df6283e4139cc0c60f693
Donny09 <127934968+Donny09@users.noreply.github.com>
2023-03-15 14:41:02 +0600

    HW3 Done

---
+++ HW3_Daniyar/main.js
@@ -0,0 +1,23 @@
+// Задание 2
+
+//console.log((prompt().length))
+
+
+// Задание 3
+// var numb1 = parseInt(prompt('первое число'))
+// var lang = prompt ('оператор(+,-,/,*)')
+// var numb2 = parseInt(prompt('второе число'))
+// switch(lang) {
+//     case '+':
+//         alert (numb1 + numb2)
+//         break;
+//         case '-':
+//         alert (numb1 - numb2)
+//         break;
+//         case '/':
+//         alert (numb1 / numb2)
+//         break;
+//         case '*':
+//         alert (numb1 * numb2)
+//         break;
+// }
